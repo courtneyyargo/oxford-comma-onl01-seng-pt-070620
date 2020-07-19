9da9937c14dba3a1302f9b2f47cdffb8d5829164
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-19 21:49:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.first
+  elsif array.size == 2 
+    array.insert(1, "and").join(" ")
+  elsif array.size == 3
+    array.last.insert(-10, "and ")
+    array.join(", ")
+  elsif array.size > 3
+    array[-1]=", and #{array[-1]}"
+  end
 end
